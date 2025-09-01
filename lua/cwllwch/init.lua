998f019228607ab39a9f47444146dc8a20320846
cwllwch <bernardo.corais@gmail.com>
2025-09-01 11:40:40 +0300

    new commit

--- lua/cwllwch/init.lua
+++ lua/cwllwch/init.lua
@@ -2,11 +2,16 @@ require("cwllwch.remap")
 
 vim.o.termguicolors=true
 require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin-frappe"
+vim.cmd.colorscheme "catppuccin-mocha"
 
 require('undotree').setup()
 
 require('lualine').setup()
+
 require('lualine').get_config()
+
+vim.lsp.config('elixirls', {
+		cmd = { '/Users/bernardo.corais/elixir-ls/release/language_server.sh' }, })
+vim.lsp.enable('elixirls')
 
 print("hello, cwllwch")
--- lua/cwllwch/packer.lua
+++ lua/cwllwch/packer.lua
@@ -7,6 +7,10 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
+  use 'prabirshrestha/vim-lsp'
+  use 'mfussenegger/nvim-dap'
+
+
   use(
   	{ "catppuccin/nvim", 
   		as = "catppuccin",
