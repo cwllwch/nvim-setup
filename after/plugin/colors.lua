0c29e511cc5af2af133951fa89e6824fdcdf7736
cwllwch <162603682+cwllwch@users.noreply.github.com>
2025-11-06 10:59:06 +0200

    added absolute number to current relative cursor line number

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -53,4 +53,5 @@ vim.api.nvim_set_hl(0, 'LineNr', { fg = "white" })
 vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = "gray" })
 vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = "gray" })
 vim.opt.relativenumber = true
-
+vim.opt.nu = true
+vim.o.statuscolumn = "%s %l %r"
